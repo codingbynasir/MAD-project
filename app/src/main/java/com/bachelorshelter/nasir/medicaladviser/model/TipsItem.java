e9868551a69121745cb45bc8afb12cb64434601b
coding by nasir <nasirkhancse@gmail.com>
2019-04-18 01:09:49 +0600

    First commit

---
+++ app/src/main/java/com/bachelorshelter/nasir/medicaladviser/model/TipsItem.java
@@ -0,0 +1,31 @@
+package com.bachelorshelter.nasir.medicaladviser.model;
+
+public class TipsItem {
+    private String headline;
+    private String description;
+    private String tipsId;
+
+    public String getTipsId() {
+        return tipsId;
+    }
+
+    public void setTipsId(String tipsId) {
+        this.tipsId = tipsId;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
